57fecb869e97896b5451548281700307f639d6c7
AH-HA-20-gg <136143073+AH-HA-20-gg@users.noreply.github.com>
2025-01-06 12:46:21 +1100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening to Queensland Roleplay", "Playing QUEENSLAND ROLEPLAY"];
+const statusMessages = ["Listening to Queensland Roleplay", "Playing Queensland Rolplay."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
